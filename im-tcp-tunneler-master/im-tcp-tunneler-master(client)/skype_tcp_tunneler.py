e9e0fa71d48a977cbd98f15c66e39355c54a0102
QUSIR <1326714828@qq.com>
2015-06-03 13:30:21 +0800

    add project

---
+++ im-tcp-tunneler-master/im-tcp-tunneler-master(client)/skype_tcp_tunneler.py
@@ -0,0 +1,138 @@
+# -*- coding:cp936 -*-
+import sys
+import time
+import traceback
+import os
+import threading
+import signal
+
+DBG1 = 1
+DBG2 = 1
+
+import im_tcp_tunneler
+
+appsByUser = {}
+
+lock = threading.Lock()
+
+def send_xmpp_message(from_jid, to_jid, txt):
+    if 1: print 'send_xmpp_message: %s->%s, %r' % (from_jid, to_jid, txt[0:])
+    try:
+        with lock:
+            if to_jid in appsByUser:
+                uapp = appsByUser[to_jid]
+            else:
+                print 'connect to %s' % to_jid
+                uapp = appsByUser[to_jid] = app.Connect(to_jid, WaitConnected=True)
+        if DBG1: print 'send: %s: len(data)=%d' % (uapp, len(txt))
+        #循环5次重试
+        for i in range(5):
+            try:
+                uapp.write(txt)
+            except Skype4Py.SkypeAPIError, e:
+                print e, '- trying again'
+            else:
+                break
+        else:
+            #读取配置文档
+            if config_ns.get('skype_strict_errors', False):
+                global keep_running; keep_running = False
+    except:
+        traceback.print_exc()
+im_tcp_tunneler.send_xmpp_message = send_xmpp_message
+
+def get_client_jid():
+    return skype.CurrentUser.Handle
+im_tcp_tunneler.get_client_jid = get_client_jid
+
+#config_ns = im_tcp_tunneler.setup_tunnels(sys.argv[-1])'tunnels.conf'
+config_ns = im_tcp_tunneler.setup_tunnels('tunnels.conf')
+
+if 0: im_tcp_tunneler.data_coding_mode = 'raw'
+
+import Skype4Py
+
+proto = os.environ.get('SKYPE_PREFFERED_PROTO', None)
+if proto: skype = Skype4Py.Skype(Transport=proto)
+else:     skype = Skype4Py.Skype()
+
+skype.Attach()
+
+def onApplicationReceiving(app_, streams):
+    if app_.Name != app.Name: return False
+    while 1:
+        ret = False
+        for strm in list(streams):
+            ret = True
+            try:
+                try:
+                    data = strm.read()
+                except Skype4Py.errors.SkypeError, e:
+                    traceback.print_exc()
+                    if config_ns.get('skype_strict_errors', False) and 'APPLICATION: operation failed' in str(e):
+                        global keep_running; keep_running = False
+                    continue
+                if DBG1: print 'received: %s: len(data)=%d' % (strm, len(data))
+                if DBG2: print 'received: %s: %r' % (strm, data)
+                im_tcp_tunneler.handle_message(strm.PartnerHandle, skype.CurrentUser.Handle, data)
+            except:
+                traceback.print_exc()
+        if 1: break
+        if not ret: break
+    return ret
+
+print 'your skypename:', skype.CurrentUser.Handle
+
+# shutdown = False
+keep_running = True
+
+def interrupted(*args):
+    print 'interrupted (SIGHUP)'
+    # global shutdown; shutdown = True
+    global keep_running; keep_running = False
+signal.signal(signal.SIGHUP, interrupted)
+
+app = skype.Application(config_ns.get('skype_app_name', 'tcp-tunneler-1'))
+app.Create()
+    
+try:
+    try:
+        print 'app:', app
+
+        if 0:
+            import code
+            def interact():
+                code.interact(local=globals())
+                global keep_running; keep_running = False
+            t = threading.Thread(target=interact)
+            t.setDaemon(True)
+            t.start()
+
+        if 1:
+            # max xfer rate = 1/dt_min*c.bufsize
+            dt_max = 0.5
+            dt_min = 0.01
+            dt = dt_max
+
+            while keep_running:
+                # if shutdown:
+                #     keep_running = im_tcp_tunneler.get_num_of_connections() > 0
+                if onApplicationReceiving(app, app.ReceivedStreams):
+                    dt /= 4.0
+                else:
+                    dt *= 2.0
+                if dt < dt_min: dt = dt_min
+                if dt > dt_max: dt = dt_max
+                if DBG2: print dt
+                time.sleep(dt)
+        if 0:
+            skype.OnApplicationReceiving = onApplicationReceiving
+            while keep_running:
+                time.sleep(5)
+            
+    except KeyboardInterrupt:
+        print 'ctrl+c'
+finally:
+    app.Delete()
+    print '== app deleted =='
+
---
+++ im-tcp-tunneler-master/im-tcp-tunneler-master(sever)/im_tcp_tunneler.py
@@ -0,0 +1,357 @@
+
+# from wsgiref.simple_server import make_server
+# import urlparse
+import threading
+import time
+import uuid
+import traceback
+import socket
+import struct
+import pprint
+
+try:
+    import gnupg
+    from M2Crypto import EVP, Rand
+except ImportError:
+    gnupg = None
+    gpg = None
+
+PROMPT = object()
+AGENT = object()
+    
+exposed = {
+    # 'IP:PORT': ['JID', ...], ...
+    }
+
+forwarded = [
+    # 'IP:PORT->IP:PORT!JID', ...
+    ]
+
+gpg_keys = [
+    # 'JID' or ('JID', 'keyid'), ...
+    ]
+
+gpg_passphrase = None
+
+encryption_is_strict = True # set this to False to allow combination of encrypted and unencrypted connections
+
+web_port = None
+
+DBG1 = 0
+DBG2 = 0
+
+BUFFERING = 1
+
+def send_xmpp_message(from_jid, to_jid, body): raise NotImplemented
+def get_client_jid(): raise NotImplemented
+
+# def web_app(environ, start_response):
+#     q = urlparse.parse_qs(environ['QUERY_STRING'])
+
+#     if '/test' in environ['PATH_INFO']:
+#         start_response('200 OK', [('Content-Type', 'text/html')])
+#         return ['OK: %s\n' % time.asctime()]
+
+#     else:
+#         start_response('404 Not Found', [('Content-Type', 'text/plain')])
+#         return ['Not Found\n']
+
+class Connection:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.sock = None
+        self.remote_jid = None
+        self.encipher = None
+        self.decipher = None
+        self.buffer = ''
+        self.bufsize = 15000
+    def __repr__(self):
+        return '%s(id=%r, remote_jid=%r)' % (self.__class__.__name__, self.id, self.remote_jid)
+    
+conns = {}
+
+def get_num_of_connections():
+    with lock: return len(conns)
+
+lock = threading.RLock()
+
+def s2x_socket_listener((src_addr, src_port), (dst_addr, dst_port), dst_jid):
+    sock = socket.socket()
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((src_addr, src_port))
+    sock.listen(5)
+    while 1:
+        conn, addr = sock.accept()
+        if DBG1:
+            print 's2x_socket_listener(%s:%d->%s:%d!%s), accepted: %r' % (
+                src_addr, src_port, dst_addr, dst_port, dst_jid, (conn, addr))
+        c = Connection()
+        c.sock = conn
+        c.remote_jid = dst_jid
+        with lock:
+            conns[c.id] = c
+            if DBG2: print conns
+        if get_jid_keyid(c.remote_jid):
+            print 'im_tcp_tunneler: connection to %s:%d!%s ENCRYPTED' % (dst_addr, dst_port, dst_jid)
+            enkey = Rand.rand_bytes(64)
+            c.encipher = EVP.Cipher(alg='aes_256_cbc', key=enkey[0:32], iv=enkey[32:64], op=1, padding=0)
+            ek = encode(encrypt_gpg(c.remote_jid, enkey))
+        else:
+            ek = '-'
+        send_xmpp_message(get_client_jid(), c.remote_jid,
+                          'CONNECT %s:%d %s %s' % (dst_addr, dst_port, c.id, ek))
+        t = threading.Thread(target=connection_handler, args=(c,))
+        t.setDaemon(True)
+        t.start()
+
+def connection_handler(c):
+    def send_data(data):
+        if c.encipher:
+            data = struct.pack('!I', len(data)) + data
+            if len(data)%16 > 0: data += Rand.rand_bytes(16 - (len(data)%16))
+            data = c.encipher.update(data)+c.encipher.final()
+        send_xmpp_message(get_client_jid(), c.remote_jid,
+                          'DATA %s %s' % (c.id, encode(data)))
+    if BUFFERING: c.sock.settimeout(0.01)
+    else:         c.sock.settimeout(0.3)
+    while 1:
+        try: d = c.sock.recv(1024)
+        except socket.timeout:
+            if DBG2: print 'socket.timeout', c.id
+            if BUFFERING:
+                # flush
+                if c.buffer:
+                    d = c.buffer; c.buffer = ''
+                    send_data(d)
+            continue
+        except socket.error:
+            if DBG1: traceback.print_exc()
+            break
+        if DBG2: print 'd: %r' % d
+        if not d: break
+        if not BUFFERING:
+            send_data(d)
+        else:
+            c.buffer += d
+            if len(c.buffer) > c.bufsize:
+                d = c.buffer[0:c.bufsize]
+                c.buffer = c.buffer[c.bufsize:]
+                send_data(d)
+    try:
+        c.sock.close()
+        c.sock.recv(1024)
+    except socket.error:
+        if DBG1: traceback.print_exc()
+    with lock:
+        if c.id in conns:
+            del conns[c.id]
+            if DBG2: print 'del conns[%r]' % c.id
+        if DBG2: print conns
+    if BUFFERING:
+        # flush
+        d = c.buffer; c.buffer = ''
+        send_data(d)
+    send_xmpp_message(get_client_jid(), c.remote_jid,
+                      'CLOSE %s' % (c.id))
+
+def parse_addr_port(addr_port):
+    addr, port = addr_port.split(':')
+    port = int(port)
+    return addr, port
+    
+def setup_accept_and_forward():
+    # accept local connections and forward them to remote tunneler
+    for s in forwarded:
+        src_addr_port, dst = s.split('->')
+        dst_addr_port, dst_jid = dst.split('!')
+        t = threading.Thread(target=s2x_socket_listener, args=(
+                parse_addr_port(src_addr_port),
+                parse_addr_port(dst_addr_port), dst_jid))
+        t.setDaemon(True)
+        t.start()
+
+def handle_message(from_jid, to_jid, body):
+    try:
+        resp = None
+
+        if 0: print 'handle_message: %s->%s, %r' % (from_jid, to_jid, body[0:])
+        print body
+        if body.startswith('CONNECT '):
+            _, addr_port, conn_id, ek = body.split(' ')
+            from_jid_norecource , _ = from_jid.split('/') # no need to specify the Recource in .conf files, or if not then no error occur
+            
+            c = Connection()
+            c.id = conn_id # needed in line 216 if errors occur
+            
+            if addr_port in exposed and ('*' in exposed[addr_port] or from_jid in exposed[addr_port] or from_jid_norecource in exposed[addr_port]): # also chek if jid without Recource is in .conf
+                print 'im_tcp_tunneler: connection allowed for %s to %s' % (from_jid, addr_port)
+                addr, port = addr_port.split(':')
+                port = int(port)
+
+                
+                c.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                c.sock.connect((addr, port))
+                c.remote_jid = from_jid
+
+                if get_jid_keyid(c.remote_jid):
+                    print 'im_tcp_tunneler: connection from %s to %s ENCRYPTED' % (from_jid, addr_port)
+                    dekey = decrypt_gpg(c.remote_jid, decode(ek))
+                    c.decipher = EVP.Cipher(alg='aes_256_cbc', key=dekey[0:32], iv=dekey[32:64], op=0, padding=0)
+                    enkey = Rand.rand_bytes(64)
+                    c.encipher = EVP.Cipher(alg='aes_256_cbc', key=enkey[0:32], iv=enkey[32:64], op=1, padding=0)
+                    ek2 = encode(encrypt_gpg(c.remote_jid, enkey))
+                else:
+                    ek2 = '-'
+
+                with lock:
+                    conns[c.id] = c
+                    if DBG2: print conns
+
+                t = threading.Thread(target=connection_handler, args=(c,))
+                t.setDaemon(True)
+                t.start()
+
+                resp = 'CONNECT_RESULT %s OK %s' % (c.id, ek2)
+
+            else:
+                if DBG1:
+                    print 'im_tcp_tunneler: connection refused for %s to %s' % (from_jid, addr_port) #useful for debugging which adress gets refused
+                resp = 'CONNECT_RESULT %s ERROR -' % c.id
+
+        elif body.startswith('CONNECT_RESULT '):
+            _, conn_id, status, ek = body.split(' ')
+
+            with lock:
+                c = conns.get(conn_id, None)
+                if DBG2: print conns
+            if c:
+                if status == 'OK':
+                    if get_jid_keyid(c.remote_jid):
+                        dekey = decrypt_gpg(c.remote_jid, decode(ek))
+                        c.decipher = EVP.Cipher(alg='aes_256_cbc', key=dekey[0:32], iv=dekey[32:64], op=0, padding=0)
+                else:
+                    c.sock.close()
+                    if DBG2: print 'sock.close:', c
+
+        elif body.startswith('CLOSE '):
+            _, conn_id = body.split()
+            with lock:
+                c = conns.get(conn_id, None)
+                if DBG2: print conns
+            if c:
+                c.sock.close()
+                if DBG2: print 'sock.close:', c
+                
+        elif body.startswith('DATA '):
+            a = body.find(' ')+1
+            b = body.find(' ', a)
+            conn_id = body[a:b]
+            data = body[b+1:]
+            with lock:
+                c = conns.get(conn_id, None)
+                if DBG2: print conns
+            if c:
+                data = decode(data)
+                if c.decipher:
+                    data = c.decipher.update(data)+c.decipher.final()
+                    hsz = struct.calcsize('!I')
+                    size, = struct.unpack('!I', data[0:hsz])
+                    data = data[hsz:hsz+size]
+                    print data
+                if DBG2: print 'data: %r' % data
+                c.sock.send(data)
+                #time.sleep(0.01)
+                # resp = 'DATA_RESULT %s OK' % conn_id
+            else:
+                resp = 'DATA_RESULT %s ERROR unknown connection id' % conn_id
+
+        if resp is not None:
+            send_xmpp_message(to_jid, from_jid, resp)
+        else:
+            # send_xmpp_message(to_jid, from_jid, 'ACK')
+            pass
+    except:
+        traceback.print_exc()
+
+def setup_tunnels(filename):
+    ns = {}
+    execfile(filename, ns)
+
+    globals().update(ns)
+
+    if gpg_keys:
+        global gpg, gpg_passphrase
+        if gpg_passphrase == PROMPT:
+            import getpass
+            gpg_passphrase = getpass.getpass('GPG passphrase: ')
+        if gpg_passphrase == AGENT:
+            gpg = gnupg.GPG(use_agent=True)
+            gpg_passphrase = None
+        else:
+            gpg = gnupg.GPG()
+            
+    setup_accept_and_forward()
+
+    # if web_port is not None:
+    #     httpd = make_server('', web_port, web_app)
+    #     t = threading.Thread(target=httpd.serve_forever)
+    #     t.setDaemon(True)
+    #     t.start()
+
+    return ns
+
+data_coding_mode = 'hex' # for xmpp
+
+def encode(data):
+    if data_coding_mode == 'hex':
+        return data.encode('hex')
+    elif data_coding_mode == 'raw':
+        return data
+    else:
+        raise RuntimeError('unknown coding %r' % data_coding_mode)
+
+def decode(data):
+    if data_coding_mode == 'hex':
+        return data.decode('hex')
+    elif data_coding_mode == 'raw':
+        return data
+    else:
+        raise RuntimeError('unknown coding %r' % data_coding_mode)
+
+def get_jid_keyid(jid):
+    for k in gpg_keys:
+        if isinstance(k, str) and k == jid:
+            return jid
+        elif isinstance(k, tuple) and k[0] == jid:
+            return k[1]
+    else:
+        return None
+    if encryption_is_strict:
+        raise RuntimeError('no key for %r' % jid)
+    else:
+        return None
+    
+def _encrypt_gpg(to_jid, data):
+    pub_keys = gpg.list_keys()
+    keyfps = [k['fingerprint'] for k in pub_keys if ('<%s>' % to_jid) in str(k['uids'])]
+    if not keyfps: raise RuntimeError('unknown gpg key')
+    if len(keyfps) != 1: raise RuntimeError('more then one pub keys found')
+    d = gpg.encrypt(data, keyfps[0], always_trust=True, armor=True)
+    return d.data
+
+def _decrypt_gpg(from_jid, data):
+    d = gpg.decrypt(data, always_trust=True, passphrase=gpg_passphrase)
+    return d.data
+
+def encrypt_gpg(jid, data):
+    keyid = get_jid_keyid(jid)
+    if keyid: data = _encrypt_gpg(keyid, data)
+    elif encryption_is_strict: raise RuntimeError('unencrypted connections are not allowed')
+    return data
+
+def decrypt_gpg(jid, data):
+    keyid = get_jid_keyid(jid)
+    if keyid: data = _decrypt_gpg(keyid, data)
+    elif encryption_is_strict: raise RuntimeError('unencrypted connections are not allowed')
+    return data
+
